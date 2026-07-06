5a9c4aa24c5c4753e79267caa9e92c3f36ac51f3
jjac02 <jjac02.jc@gmail.com>
2026-07-06 11:41:04 +0800

    relocated logout to auth, updated documentation

--- syllabux_backend/src/routes/auth.js
+++ syllabux_backend/src/routes/auth.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import * as authController from '../controllers/auth.js';
+import { auth } from '../middleware/auth.js';
 
 const router = Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/logout', auth, authController.logout);
 router.post('/validatetoken', authController.tokenValidator);
 export default router;
--- syllabux_backend/src/routes/users.js
+++ syllabux_backend/src/routes/users.js
@@ -6,6 +6,5 @@ const router = Router();
 router.post('/', usersController.create);
 router.put('/:id', usersController.update);
 router.delete('/:id', usersController.remove);
-router.post('/logout', usersController.logout);
 
 export default router;
